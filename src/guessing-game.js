80e25d37010b0e19b999cbe4d465c8f9b6f44469
Zhenokin <chechenyshka@mail.ru>
2017-09-14 18:11:08 +0300

    create game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = 0;
+        this.min = 0;
+        this.res = 0;
+
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.res = Math.ceil((this.min + this.max) / 2);
+        return this.res;
     }
 
     lower() {
-
+        this.max = this.res;
     }
 
     greater() {
-
+        this.min = this.res;
     }
 }
 
